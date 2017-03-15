681bff4e64d554367e4650ad60ac909cafa6e5e3
Andreas Strand <anstra@stud.ntnu.no>
2017-03-15 09:21:33 +0100

    adding result to entities

---
+++ src/entities/Resultat.java
@@ -0,0 +1,19 @@
+package entities;
+
+/**
+ * Created by Andreas on 15/03/2017.
+ */
+public class Resultat {
+
+    private int belastning;
+    private int antReps;
+    private int antSett;
+
+    public int getBelastning() {
+        return belastning;
+    }
+
+    public void setBelastning(int belastning) {
+        this.belastning = belastning;
+    }
+}
