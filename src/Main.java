9cd42c2585d8be5347920a34e2d70004ae546d7d
TrulsElg <trulsel@stud.ntnu.no>
2017-03-16 12:18:36 +0100

    Added new menu option "fjern øvelse"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import com.util.DatabaseManager;
-import com.util.ObjectAdder;
-import com.util.TableCreator;
-import com.util.TableManager;
+import com.util.*;
 import implementations.PersonDatabase;
 import java.sql.Date;
 import java.util.ArrayList;
@@ -17,8 +14,7 @@ public class Main {
 
 
     public static void main(String[] args) {
-		TableCreator tblc = new TableCreator();
-		tblc.createTables("prosjekt");
+
 		DatabaseManager dbc = new DatabaseManager();
         //dbc.createDatabase("Test");
         //dbc.deleteDatabase("Test");
@@ -32,8 +28,12 @@ public class Main {
 		//tbm.makeQuery("Test","SELECT id, first_name, last_name FROM person");
 		//Main tb = new Main();
 		//tbm.dropTable("prosjekt", "resultater");
+		//tbm.dropTable("prosjekt", "treningsokt");
 		tbm.deleteID("prosjekt", "ovelse",3);
 		//tbm.makeQuery("Test","SELECT id, first_name, last_name FROM person");
+
+		TableCreator tblc = new TableCreator();
+		tblc.createTables("prosjekt");
 
 		ObjectAdder oa = new ObjectAdder();
 		//Ovelse ovelse = new Ovelse("Knebøy", "Skuldre over knær, gå ned til 90 grader i knærne og opp igjen i en sammenhengende bevegelse.");
@@ -46,6 +46,7 @@ public class Main {
 
 
 	public void run(){
+		Registrator reg = new Registrator();
 		System.out.println("Treningsappen æTrene");
 		System.out.println(line + "\n\n");
 		boolean runWhile=true;
@@ -54,64 +55,50 @@ public class Main {
 
 			System.out.println("Meny");
 			System.out.println(line);
-			System.out.println("1.Registrer ny økt \n2.Registrer ny øvelse\n3.Se statistikk over treningsøkter de siste 30 dagene \n4.Se progresjon for individuell øvelse \n5.Exit\n\n\n\n");
+			System.out.println("1.Registrer ny økt \n2.Registrer ny øvelse\n3.Se statistikk over treningsøkter de siste 30 dagene \n4.Se progresjon for individuell øvelse \n5.Fjern en øvelse fra databasen\n6.Exit\n\n\n\n");
 			
 			System.out.print("Skriv inn tall: ");
 			int tall = reader.nextInt();
 			
 			switch (tall) {
-			case 1:	newSession();
-			break;
-			case 2: newExercise();
-			break;
-			case 3: statistics();
-			break;
-			case 4: progression();
-			break;
-			case 5: 
-				runWhile=false;
-				System.out.println("Programmet er avsluttet. Takk for at du bruker æTrene!");
-				break;
+				case 1:	reg.nyOkt();
+					break;
+				case 2: reg.newExercise();
+					break;
+				case 3: statistics();
+					break;
+				case 4: progression();
+					break;
+				case 5:
+					System.out.println("Du vil slette en øvelse");
+					break;
+				case 6:
+					runWhile=false;
+					System.out.println("Programmet er avsluttet. Takk for at du bruker æTrene!");
+					break;
 			} //End Switch
 			
 		}//End While
 	}// Run
 		
-		public void newExercise(){
-			Scanner scanner = new Scanner(System.in);
 
-			System.out.println("-------------------");
-			System.out.println("Registrer ny øvelse");
-			System.out.println("-------------------\n");
-
-			System.out.print("Skriv navnet til øvelsen: ");
-			String navn = scanner.nextLine();
-			//System.out.println("Du ga øvelsen navnet: " + navn);
-
-			System.out.print("Gi øvelsen en beskrivelse: ");
-			String beskrivelse = scanner.nextLine();
-			//System.out.println("Du la til beskrivelsen: '"+beskrivelse+"'");
-
-			//String[] exercise = reader.nextLine().split(",");
-			Ovelse ovelse = new Ovelse(navn, beskrivelse);
-
-			ObjectAdder oa = new ObjectAdder();
-			oa.addOvelse(ovelse);
-
-			System.out.println("Du har oprettet øvelsen '" + ovelse.getNavn() + "' med beskrivelsen:\n" + ovelse.getBeskrivelse());
-			System.out.println("\n------------------------------\n");
-		}
 		
 		public void newSession(){
+			Scanner input = new Scanner(System.in);
+
 			Calendar start = Calendar.getInstance();
 
-			
-			
-			System.out.println("Skriv inn dagsform for treningsøkten som et tall 1-10:\n");
-			int dagsform = reader.nextInt();
+			System.out.print("Skriv inn dagsform for treningsøkten som et tall 1-10: ");
+			int dagsform = input.nextInt();
+			System.out.println("Dagsform: "+dagsform);
+
+			System.out.print("Legg til notat om treningsøkten: ");
+			String notat = input.nextLine();
+			System.out.println("Du la til notatet '"+notat+"'");
 
 			boolean runWhile =  true;
 			ArrayList<Resultat> results = new ArrayList<Resultat>();
+			/*
 			while(runWhile){
 				System.out.println("Tast inn nr for øvelse i databasen du ønsker å legge til denne økten:\n"
 						+ line + "\n" +testOvelser); //TODO henting fra SQL
@@ -123,8 +110,9 @@ public class Main {
 
 				results.add(new Resultat(Integer.parseInt(enkeltResultat[0]),Integer.parseInt(enkeltResultat[1]),Integer.parseInt(enkeltResultat[2]))); //int belastning, int antSett, int antReps
 			} //End While
+			*/
 			Calendar slutt = Calendar.getInstance();
-			Treningsokt okt = new Treningsokt(dagsform,results, start, slutt );
+			//Treningsokt okt = new Treningsokt(dagsform,results, start, slutt );
 
 		}
 		public void statistics(){
